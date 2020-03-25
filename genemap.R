a3734af87a4eb1ad2b627cfa7402bc0911d6c63d
Artem Sokolov <artem.sokolov@gmail.com>
2020-03-25 17:15:06 -0400

    Made genemap into a stand-alone Rscript

--- genemap.R
+++ genemap.R
@@ -2,27 +2,32 @@
 ##
 ## by Artem Sokolov
 
-library( tidyverse )
+## Parse version number from the command line
+argv <- commandArgs(TRUE)
+if( length(argv) != 1 )
+    stop( "Usage: Rscript genemap.R [build version]" )
+v <- argv[1]
+
+suppressMessages(library( tidyverse ))
 
 ## Location of the latest .gtf release
-fnGTF <- "ftp://ftp.ensembl.org/pub/release-97/gtf/homo_sapiens/Homo_sapiens.GRCh38.97.gtf.gz"
+fnGTF <- str_c("ftp://ftp.ensembl.org/pub/release-", v,
+               "/gtf/homo_sapiens/Homo_sapiens.GRCh38.", v,
+               ".gtf.gz")
 
-main <- function()
-{
-    ## Parse the raw .gtf file
-    cn <- c( "seqname", "source", "feature", "start", "end", "score", "strand", "frame", "attribute" )
-    ct <- cols( .default = col_character(), start = col_integer(), end = col_integer() )
-    X <- read_tsv( fnGTF, comment="#!", col_types=ct, col_names = cn )
+## Parse the raw .gtf file
+cn <- c( "seqname", "source", "feature", "start", "end", "score", "strand", "frame", "attribute" )
+ct <- cols( .default = col_character(), start = col_integer(), end = col_integer() )
+X <- read_tsv( fnGTF, comment="#!", col_types=ct, col_names = cn )
 
-    ## Isolate the gene entries and compute gene lengths
-    Y <- X %>% filter( feature == "gene" ) %>% mutate( gene_length = end-start+1 )
+## Isolate the gene entries and compute gene lengths
+Y <- X %>% filter( feature == "gene" ) %>% mutate( gene_length = end-start+1 )
 
-    ## Parse key-value pairs
-    G <- Y$attribute %>% str_sub(1,-2) %>% str_split("; ") %>% map(str_split, " ") %>%
-        map( ~set_names(map(.x, pluck, 2), map(.x, pluck, 1)) ) %>%
-        modify_depth( 2, str_sub, 2, -2 ) %>%
-        map2( Y$gene_length, ~c(.x, list(gene_length=.y)) ) %>% bind_rows()
+## Parse key-value pairs
+G <- Y$attribute %>% str_sub(1,-2) %>% str_split("; ") %>% map(str_split, " ") %>%
+    map( ~set_names(map(.x, pluck, 2), map(.x, pluck, 1)) ) %>%
+    modify_depth( 2, str_sub, 2, -2 ) %>%
+    map2( Y$gene_length, ~c(.x, list(gene_length=.y)) ) %>% bind_rows()
     
-    ## Save a copy to a file
-    G %>% write_csv( "GRCh38.97.genemap.csv" )
-}
+## Save a copy to a file
+G %>% write_csv( str_c("GRCh38.",v,".genemap.csv") )
